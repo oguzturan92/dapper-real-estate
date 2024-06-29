e7afe881b4a260d904db933b8b0d094630033185
Oğuzhan Turan <oguzhan_turan_52@hotmail.com>
2024-06-29 10:13:22 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DapperRealEstate.Models;
+
+namespace DapperRealEstate.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
